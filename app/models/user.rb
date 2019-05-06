9efbd1997424291499fb68549e9c40c2e4802d53
federicocavazzoli <fedecavazzoli@gmail.com>
2019-05-06 09:28:51 -0300

    Fixing PR rubocop.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   extend Devise::Models
   validates :first_name, presence: true
   validates :last_name, presence: true
