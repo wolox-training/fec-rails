16b973e9686c61da5e90d525dc0e20cfe6c1a8a9
Federico Cavazzoli <federico.cavazzoli@wolox.com.ar>
2019-05-07 11:26:50 -0300

    fixing PR

--- db/migrate/20190430212446_create_books.rb
+++ db/migrate/20190430212446_create_books.rb
@@ -1,12 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.string :genre
-      t.string :author
-      t.string :image
-      t.string :title
-      t.string :editorial
-      t.string :year
+      t.string :genre, :null => false, :default => Faker::Book.genre
+      t.string :author, :null => false, :default => Faker::Book.author
+      t.string :image, :null => false, :default =>  Faker::File.file_name('foo/bar', 'baz', 'jpg')
+      t.string :title, :null => false, :default =>  Faker::Book.title
+      t.string :editorial, :null => false, :default => Faker::Book.publisher
+      t.string :year, :null => false, :default => Faker::Date.birthday 
 
       t.timestamps
     end
