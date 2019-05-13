cc5730e2f2df29a3f52af011041cb815ade24354
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-13 15:05:59 -0300

    chech autenticated user but ugly coding

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe BooksController, type: :controller do
-
+describe Api::V1::BooksController, type: :controller do
+  describe 'GET BooksController#index' do
+    let(:book) {FactoryBot.create :book, 5}
+    it "get index is valid request when user is signed_in" do
+      m = create(:user)
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.headers.merge! m.create_new_auth_token
+      sign_in m
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
 end
