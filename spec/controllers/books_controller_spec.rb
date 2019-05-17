ac815b4cfab91b6c8c7ac931967494b72f45d6bd
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-17 14:41:08 -0300

    routes Book#index and Book#show are tested.

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,66 +1,43 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-include ActionController::RespondWith
 
 describe Api::V1::BooksController, type: :controller do
-  describe 'GET BooksController#index' do
-    let(:book) {FactoryBot.create :book}
-    before(:each) do
-      @current_user = FactoryBot.create(:user)
-    end
-    context 'it gives you autenticates headers'
-      it "gives you an authentication code if you are an existing user and you satisfy the password" do
-        login
-        # puts "#{response.headers.inspect}"
-        # puts "#{response.body.inspect}"
-        expect(response.has_header?('access-token')).to eq(true)
-        end
-      end
-    context 'When fetching all the books when user is logged in' do
-      before do
-        login
-        auth_params = get_auth_params_from_login_response_headers(response)
-        get :index, params: auth_params
-      end
-
-      it 'responses with books json' do
-        expected = ActiveModel::Serializer.new(book, each_serializer: BookSerializer).to_json
-        expect(response) =~ JSON.parse(expected)
-      end
-
-    end
-
-    context 'when user is not logged in' do
-      before do
+  describe 'GET #index' do
+    let!(:books) { create_list(:book, 3) }
+    context 'When fetching all the books in the datebase with authenticate_user' do
+      include_context 'Authenticated User'
+      subject(:http_request) do
         get :index
       end
 
-      it 'Does not response with book list' do
-        expect(response).to eq(200)
+      it 'responses with the users book json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(books, each_serializer: BookSerializer).to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
       end
     end
- end
+  end
 
-#-----------------------------------------------------------
+  describe 'GET #show' do
+    context 'When fetching a book' do
+      let!(:book) { create_list(:book, 1) }
+      subject(:http_request) do
+        get :show, params: { id: book.id }
+      end
 
-def login
-    post '/auth/sign_in/', params: { email: @current_user.email, password: @current_user.password}
-end
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
 
-def get_auth_params_from_login_response_headers(response)
-      client = response.headers['client']
-      token = response.headers['access-token']
-      expiry = response.headers['expiry']
-      token_type = response.headers['token-type']
-      uid =   response.headers['uid']
+      it 'responses with the book' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(book, each_serializer: BookSerializer).to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
 
-      auth_params = {
-                      'access-token' => token,
-                      'client' => client,
-                      'uid' => uid,
-                      'expiry' => expiry,
-                      'token_type' => token_type
-                    }
-      auth_params
+    end
+  end
 end
