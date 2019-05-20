e18b25fd66fe96a0252ebd93c04f9d77efa0ffa3
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-20 10:51:54 -0300

    enpoints created

---
+++ app/controllers/api/v1/rents_controller.rb
@@ -0,0 +1,7 @@
+class RentsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'rents/create'
+
+  get 'rents/index'
+
   namespace 'api' do
     namespace 'v1' do
       resources :books
       get 'api/v1/books/show', to: 'books#show', controller: 'books'
       get 'api/v1/books/index', to: 'books#index', controller: 'books'
+      post 'api/v1/rents/create', to: 'rents#create', controller:'rents'
+      get 'api/v1/rents/index', to: 'rents#index', controller:'rents'
     end
   end
   mount_devise_token_auth_for 'User', at: '/auth'
