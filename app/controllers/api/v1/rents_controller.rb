cb0da32fe39c1eccf4e1ec23e8cba80a250db6b6
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-20 13:52:25 -0300

    not working not sure why

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 module Api
   module V1
@@ -5,11 +6,16 @@ module Api
       include Wor::Paginate
       before_action :authenticate_user!
       def create
-
+        unless params[:book_id].presence? && params[:start_at].presence? && params[:end_at].presence?
+          @rent = Rent.create(user: User.where(uid: headers[:uid]), book: params[:book_id], start_at: params[:start_at], end_at: params[:end_at])
+          render json: @rent
+        else
+          render json: {status: "error", code: 3000, message: "Params missing."}
+        end
       end
 
       def index
-        current_user: params[:user]
+        current_user = User.where(uid: headers[:uid])
         @rent = current_user.rents
         render_paginated @rent, each_serializer: RentSerializer
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'rents/create'
-
-  get 'rents/index'
-
   namespace 'api' do
     namespace 'v1' do
       resources :books
-      get 'api/v1/books/show', to: 'books#show', controller: 'books'
-      get 'api/v1/books/index', to: 'books#index', controller: 'books'
-      post 'api/v1/rents/create', to: 'rents#create', controller:'rents'
-      get 'api/v1/rents/index', to: 'rents#index', controller:'rents'
+      get 'books/show', to: 'books#show', controller: 'books'
+      get 'books/index', to: 'books#index', controller: 'books'
+      resources :rents
+      post 'rents/create', to: 'rents#create', controller:'rents'
+      get 'rents/index', to: 'rents#index', controller:'rents'
     end
   end
   mount_devise_token_auth_for 'User', at: '/auth'
