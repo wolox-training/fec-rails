0774fe6e23179e04ba019a9773164fe05c2b6ab0
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-27 19:02:42 -0300

    comment future changes

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,13 +7,13 @@ module Api
       include DeviseTokenAuth::Concerns::SetUserByToken
       before_action :authenticate_user!
       def index
-        @book = Book.all
-        render_paginated @book, each_serializer: BookSerializer
+        book = Book.all
+        render_paginated book, each_serializer: BookSerializer
       end
 
       def show
-        @book = Book.find(params[:id])
-        render json: @book
+        book = Book.find(params[:id])
+        render json: book
       end
     end
   end
--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -7,23 +7,25 @@ module Api
       include DeviseTokenAuth::Concerns::SetUserByToken
       before_action :authenticate_user!
       # rubocop:disable Metrics/AbcSize
-      def create
+      def create  #add method user and book find
         if params.key?(:book_id) && params.key?(:start_at) && params.key?(:end_at)
-          user = User.find_by email: request.headers[:uid]
+          user = User.find_by email: request.headers[:uid] #user current_user DTA
           book = Book.find_by id: params[:book_id]
-          @rent = Rent.create(user: user, book: book, start_at: params[:start_at], end_at: params[:end_at])
-          render json: @rent
-          MailSenderWorker.perform_async('new_rent')
+          rent = Rent.create(user: user, book: book, start_at: params[:start_at], end_at: params[:end_at])
+          if rent
+            MailSenderWorker.perform_async('new_rent') #hacer un deliver_later
+          end #emprolijar a una linea
+          render json: rent
         else
-          render json: { status: 'error', code: 3000, message: 'Params missing.' }
+          render json: { status: 'error', code: 422, message: 'Params missing.' }
         end
       end
       # rubocop:enable Metrics/AbcSize
 
       def index
         current_user = User.find_by email: request.headers[:uid]
-        @rent = current_user.rents
-        render_paginated @rent, each_serializer: RentSerializer
+        rent = current_user.rents
+        render_paginated rent, each_serializer: RentSerializer
       end
     end
   end
