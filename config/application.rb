566b5ee8eaf9fa15e24c81152569933cc4bf5aaa
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-27 12:21:44 -0300

    PR needs

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+require 'dotenv'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
