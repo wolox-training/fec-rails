4e52aafc0e9e748be62a8a36fe5cb50ed90b5443
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-05-27 17:20:40 -0300

    sidekiq calls Mailer but it is not working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,13 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
 
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -51,4 +54,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  user_name: ENV['user_name'],
+  password: ENV['password'],
+  address: ENV['address'],
+  domain: ENV['domain'],
+  port: ENV['port'],
+  authentication: 'cram_md5'
+}
+
 end
