566f3626bc5fc72266f356cf4e504f5a4e964f65
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-06-03 18:13:02 -0300

    controller implemetation not working

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :books, only: [:show, :index]
       resources :rents, only: [:create, :index]
+      resources :booksuggestion, only: [:create]
     end
   end
 end
