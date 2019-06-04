5e3d14448398db9037c6ddcca79eaecef6f33800
Federico Cavazzoli <federicocavazzoli@MacBook-Pro-de-Federico.local>
2019-06-04 13:21:56 -0300

    rubocop failures

--- spec/controllers/rents_controller_spec.rb
+++ spec/controllers/rents_controller_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
- describe Api::V1::RentsController, type: :controller do
+describe Api::V1::RentsController, type: :controller do
   include_context 'Authenticated User'
   describe 'GET #create' do
     it 'returns http success' do
